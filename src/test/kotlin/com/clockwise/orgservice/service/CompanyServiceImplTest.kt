499c630c67cceac6b32d329701fe74b82e563793
Martin Hristev <mhristev03@gmail.com>
2025-03-03 22:30:26 +0200

    update to working test

--- src/test/kotlin/com/clockwise/orgservice/service/CompanyServiceImplTest.kt
+++ src/test/kotlin/com/clockwise/orgservice/service/CompanyServiceImplTest.kt
@@ -54,7 +54,7 @@ class CompanyServiceImplTest @Autowired constructor(
 
         val savedCompany = assertSuccess(result)
         assertNotNull(savedCompany?.id)
-        assertEquals(newCompany.name, savedCompany?.name + "asd")
+        assertEquals(newCompany.name, savedCompany?.name)
         assertEquals(newCompany.description, savedCompany?.description)
     }
 
