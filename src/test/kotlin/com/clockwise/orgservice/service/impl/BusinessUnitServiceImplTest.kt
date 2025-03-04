e0969c0179f696b7cd0524f1e0bfd3dc09534707
Martin Hristev <mhristev03@gmail.com>
2025-03-04 16:53:22 +0200

    test containers problem not sharing

--- src/test/kotlin/com/clockwise/orgservice/service/impl/BusinessUnitServiceImplTest.kt
+++ src/test/kotlin/com/clockwise/orgservice/service/impl/BusinessUnitServiceImplTest.kt
@@ -12,17 +12,55 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 @SpringBootTest
+@Testcontainers
 @ActiveProfiles("test")
 class BusinessUnitServiceImplTest @Autowired constructor(
  private val businessUnitService: BusinessUnitService,
  private val companyService: CompanyService,
  private val flyway: Flyway
-) : AbstractIntegrationTest() {
+) {
+    companion object {
+        @Container
+        val postgresContainer = PostgreSQLContainer<Nothing>("postgres:14").apply {
+            withDatabaseName("test-db")
+            withUsername("test")
+            withPassword("test")
+        }
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun overrideProperties(registry: DynamicPropertyRegistry) {
+            val host = postgresContainer.host
+            val port = postgresContainer.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)
+            val database = postgresContainer.databaseName
+            val username = postgresContainer.username
+            val password = postgresContainer.password
+
+            // Override R2DBC properties
+            registry.add("spring.r2dbc.url") {
+                "r2dbc:postgresql://$host:$port/$database"
+            }
+            registry.add("spring.r2dbc.username") { username }
+            registry.add("spring.r2dbc.password") { password }
+
+            // Override Flyway properties (if using Flyway with JDBC)
+            registry.add("spring.flyway.url") {
+                "jdbc:postgresql://$host:$port/$database"
+            }
+            registry.add("spring.flyway.user") { username }
+            registry.add("spring.flyway.password") { password }
+        }
+    }
     private lateinit var testBusinessUnit: BusinessUnit
 
     @BeforeEach
